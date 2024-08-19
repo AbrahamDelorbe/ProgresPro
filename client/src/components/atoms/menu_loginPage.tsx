d270036b791e9aee73dfe701524a3c36195f3b5e
Manuel Reyes <Manueldjsr@gmail.com>
2024-08-19 18:18:22 -0400

    Update menu_loginPage.tsx

--- client/src/components/atoms/menu_loginPage.tsx
+++ client/src/components/atoms/menu_loginPage.tsx
@@ -1,9 +1,12 @@
-
+// READ THIS:
+// Remember that is a stardard in React to name the components files capitalized, meaning
+// if your component is something related to the manu, the right name is: MenuLoginPage.tsx. On the other hand,
+// also remember that we do not use underscore in naming components, it is not part of the standard.
 
 export default function MenuloginPage() {
   return (
     <div>
-
+        
 
 
     </div>
