598aabe7b244cab847871df70c16bd46061912c8
Del Orbe <abrahamdelorbe877@gmail.com>
2024-08-21 09:36:43 -0400

    prueba spam

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,16 +5,8 @@ import LoginPage from './pages/loginPage.js';
 
 export default function App() {
   return (
-    <div
-      className="bg-cover bg-center h-screen"
-      style={{
-        backgroundImage:
-          "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('https://static.fundacion-affinity.org/cdn/farfuture/PVbbIC-0M9y4fPbbCsdvAD8bcjjtbFc0NSP3lRwlWcE/mtime:1643275542/sites/default/files/los-10-sonidos-principales-del-perro.jpg')",
-      }}
-    >
-      <h1 className="text-white text-4xl font-bold text-center pt-10">
-        Hello World!
-      </h1>
+    <div>
+      
 
       <LoginPage />
     </div>
